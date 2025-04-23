3f86a810b986dade5c1b37559b38c22af2402a80
Usman0226 <chandanusmangani@gmail.com>
2025-04-23 17:24:43 +0530

    Dashboard Done

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let 
+const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('toggleBtn');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('expanded');
+});
+
